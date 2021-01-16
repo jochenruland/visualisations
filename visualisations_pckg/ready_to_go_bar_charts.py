f2b5c4db10c0e7185367ac9db85d66b79ce55a42
jochenruland <jochenhgruland@gmail.com>
2021-01-16 09:13:16 +0100

    class Customcountplot __init__ added

--- visualisations_pckg/ready_to_go_bar_charts.py
+++ visualisations_pckg/ready_to_go_bar_charts.py
@@ -3,11 +3,28 @@ import matplotlib as plt
 import pandas as pd
 
 class Customcountplot(sb.countplot):
-    def __init__(self, x, y, hue=None, data=None, order=None, hue_order=None,
-                 orient="h", color=sb.color_palette()[1], palette=None, saturation=0.75, dodge=True, ax=None, **kwargs,
-                 x_lab='category', y_lab='counts', tit='barchart for qualitative data'):
-        super().__init__(self, x, y, hue, data, order, hue_order, orient, color, palette,
-                        saturation, dodge, ax)
+    """A countplot with axis lables and title"""
+    def __init__(self, *args, x_lab='category', y_lab='counts', tit='barchart for qualitative data', **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.x_lab = x_lab
+        self.y_lab = y_lab
+        self.tit = tit
+
+        plt.xlabel(self.x_lab)
+        plt.ylabel(self.y_lab)
+        plt.title(self.tit)
+
+
+
+
+
+
+    #def __init__(self, x, y, hue=None, data=None, order=None, hue_order=None,
+    #             orient="h", color=sb.color_palette()[1], palette=None, saturation=0.75, dodge=True, ax=None, **kwargs,
+    #             x_lab='category', y_lab='counts', tit='barchart for qualitative data'):
+    #    super().__init__(self, x, y, hue, data, order, hue_order, orient, color, palette,
+    #                    saturation, dodge, ax)
         ''' you can either setup the Custom class __init__ function with all arguments
             necessary for the __init__ of the parent class and pass those values to
             the parent class OR you only pass less arguments to the __init__ of the parent class but
@@ -15,18 +32,5 @@ class Customcountplot(sb.countplot):
                         #(self, *, x, y, hue, data, order,
                         #hue_order, orient, color, palette,
                         #saturation, dodge, ax, **kwargs)
-        self.x_lab = x_lab
-        self.y_lab = y_lab
-        self.tit = tit
-        self.data = pd.series()
 
-def read_data_file(self, filename):
-    ''' Function to read in data from a .csv file. The file should have a
-        categorical value and a quantitative value per line separated by comma.
-
-        Args:
-            filname (string): name of file to read from
-        Returns:
-            None '''
-    s = pd.read_csv(filename)
-    self.data = s        
+    #    self.data = pd.series()
