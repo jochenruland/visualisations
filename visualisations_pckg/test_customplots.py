e65c2d75ccd204058a960bda74724fe276b55eef
jochenruland <jochenhgruland@gmail.com>
2021-01-16 09:15:08 +0100

    Function read_data_file and class initialisation added

---
+++ visualisations_pckg/test_customplots.py
@@ -0,0 +1,18 @@
+from ready_to_go_bar_charts import *
+import pandas as pd
+
+def read_data_file(filename):
+    ''' Function to read in data from a .csv file. The file should have a
+        categorical value and a quantitative value per line separated by comma.
+
+        Args:
+            filname (string): name of file to read from
+        Returns:
+            None '''
+    s = pd.read_csv(filename)
+    return s
+
+b_chart = Customcountplot()
+d_series = read_data_file('datafile.csv')
+
+b_chart(data=d_series)
