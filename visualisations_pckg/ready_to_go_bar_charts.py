defb2e28a6854d8e1f89d7e63dd5ee1b3fe6781d
jochenruland <jochenhgruland@gmail.com>
2021-01-26 09:31:07 +0100

    Different functions added to class Customplodata

--- visualisations_pckg/ready_to_go_bar_charts.py
+++ visualisations_pckg/ready_to_go_bar_charts.py
@@ -6,14 +6,15 @@ import pandas as pd
 
 class Customplotdata():
     """ Creates a Customplotdata object which stores information how to format different matplotlib.pyplot and seaborn plots.
-        The matplotlib.pyplot functions which can be called as methods of the Customplotdata object work in their original way
-        only adding the formatting information stored in the object"""
-    def __init__(self, x_label='category', y_label='counts', p_title='countplot', cat_order_list=None, p_figsize=[5,5], p_legend=False):
+        The matplotlib.pyplot functions, which can be called as methods of the Customplotdata object, work in their original way
+        only adding the formatting information stored in the object """
+    def __init__(self, x_label='category 1', y_label='counts', hue_label='category 2', p_title='countplot', cat_order_list=None, p_figsize=[7,4], p_legend=False):
         self.x_label=x_label
         self.y_label=y_label
         self.p_title=p_title
         self.p_figsize=p_figsize
         self.p_legend=p_legend
+        self.ordered_cat=None
 
         if cat_order_list is not None:
             self.ordered_cat = pd.api.types.CategoricalDtype(ordered=True, categories=cat_order_list)
@@ -29,18 +30,28 @@ class Customplotdata():
                 Returns:
                 None '''
         self.p_df = pd.read_csv(filename)
+        return print(self.p_df.head())
 
     def print_dtypes(self):
         print(self.p_df.dtypes)
 
-    def customcountplot(self, x):
+    def customcountplot(self, x, hue=None, order=None):
         fig=plt.figure(figsize=self.p_figsize)
-        fig.suptitle(self.p_title, fontsize=16)
+        fig.suptitle(self.p_title, fontsize=12)
         base_color = sb.color_palette()[1]
-        sb.countplot(data=self.p_df, x=x, color=base_color)
 
-cpd=Customplotdata()
-cpd.read_dataframe_fom_file('pokemon.csv')
-#test_cpd.print_dtypes()
-cpd.customcountplot('type_2')
-plt.show()
+        if self.ordered_cat is not None:
+            self.p_df[x] = self.p_df[x].astype(self.ordered_cat)
+
+
+        sb.countplot(data=self.p_df, x=x, hue=hue, color=base_color, order=order)
+        plt.xlabel(self.x_label)
+        plt.ylabel(self.y_label)
+        plt.xticks(rotation=45)
+
+
+#cpd=Customplotdata()
+#cpd.read_dataframe_fom_file('fuel-econ.csv')
+#cpd.print_dtypes()
+#cpd.customcountplot(x='make', hue='feScore')
+#plt.show()
